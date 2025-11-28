750e4c494200e85c5602329853518513e7ac4db9
Alidabi10 <112751131+Alidabi10@users.noreply.github.com>
2025-11-28 11:53:49 +0100

    Refactor foo function to use vector and implement logic
    Refactor foo function to use a new vector and implement logic to determine character based on monster status.

--- foo.hpp
+++ foo.hpp
@@ -1,16 +1,27 @@
 #pragma once
-
 #include "Human.hpp"
-
 #include <algorithm>
 #include <list>
 #include <vector>
-
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > retval(people.size());
+	std::vector<char> wynik(people.size());
 
-    // Twoja implementacja tutaj
+	auto iterator_wyniku = wynik.begin();
+	std::for_each(people.rbegin(), people.rend(), [&](Human& person) {
+		person.birthday();
+		char znak;
+		if (person.isMonster()) {
+			znak = 'n';
+		}
+		else {
+			znak = 'y';
+		}
+		*iterator_wyniku = znak;
+		iterator_wyniku++;
 
-    return retval;
+
+		});
+	
+	return wynik;
 }
